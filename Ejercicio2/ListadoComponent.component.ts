3d2be8122f0024be1860c35a5f2585879a654057
EmanuelAlexPrut <73123038+EmanuelAlexPrut@users.noreply.github.com>
2021-11-03 15:56:47 +0100

    Update ListadoComponent.component.ts

--- Ejercicio2/ListadoComponent.component.ts
+++ Ejercicio2/ListadoComponent.component.ts
@@ -1,1 +1,15 @@
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'app-listado-component',
+  templateUrl: './listado-component.component.html',
+})
+export class ListadoComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  listaHeroes: string[] = ["Superman", "Antman", "Batman", "Thor"];
+  listaEdades: number[] = [29, 33, 45, 19];
+}
